ca2340ac55356cf5157e9d75c00f8762bf7c5468
Asad Hussain <AsadHusn007@gmail.com>
2024-10-19 07:55:20 +0000

    solving

--- index.js
+++ index.js
@@ -1,18 +1,103 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const crypto = require("crypto");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const USERS = [
+  {
+    _id: "73b86379-b4a8-4722-82a3-cf41bc30f1f7",
+    username: "asad",
+  },
+];
 
+const EXERCISES = [
+  {
+    _id: "73b86379-b4a8-4722-82a3-cf41bc30f1f7",
+    username: "asad",
+    description: "desc",
+    duration: 2,
+    date: "Sat Feb 10 2024",
+  },
+];
 
+app.get("/api/users", (req, res) => {
+  return res.json(USERS);
+});
 
+app.post("/api/users", (req, res) => {
+  const username = req.body.username?.trim();
+  console.log("body", req.body);
+  if (!username) return res.status(404).send("Username required");
+  const user = {
+    _id: crypto.randomUUID(),
+    username,
+  };
+  USERS.push(user);
+  res.json(user);
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const body = { ...req.body };
+  const userId = req.params._id;
+
+  console.log("body", body);
+  const { description, duration, date } = body;
+  if (!userId || !description || !duration)
+    return res
+      .status(404)
+      .send("Please provide valid description and duration");
+
+  const user = USERS.find(({ _id }) => _id === userId);
+  if (!user) return res.status(404).send("User not exist with this userId");
+
+  if (!parseInt(duration))
+    return res.status(404).send("User not exist with this userId");
+
+  const d = new Date(date ?? Date.now()).toDateString();
+
+  if (d === "Invalid Date") return res.status(404).send("Invalid Date");
+
+  const exercise = {
+    _id: userId,
+    username: user.username,
+    description,
+    duration: parseInt(duration),
+    date: d,
+  };
+  EXERCISES.push(exercise);
+  res.json(exercise);
+});
+
+app.get("/api/users/:id/logs", (req, res) => {
+  const userId = req.params.id;
+  const user = USERS.find(({ _id }) => _id === userId);
+  if (!user) return res.status(404).send("User not exist with this userId");
+  const exercises = EXERCISES.filter(({ _id }) => _id === userId).map((e) => {
+    const exercise = { ...e };
+    delete exercise._id;
+    return exercise;
+  });
+
+  return res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: userId,
+    logs: exercises,
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
